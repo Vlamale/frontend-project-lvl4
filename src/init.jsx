2676c9f50fc81d7c07e56911fda1c22647bc0528
Vlad <kristoyt@bk.ru>
2022-04-29 18:21:26 +0300

    fix test errors 2

--- src/init.jsx
+++ src/init.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 import App from './App.jsx';
 import AppProvider from './context/app/AppProvider.jsx';
 import SocketProvider from './context/socket/SocketProvider.jsx';
 import store from './slices/index.js';
+import i18instance from './locales/i18n.js';
 
 const init = (socket) => {
   const rollbarConfig = {
@@ -24,7 +26,9 @@ const init = (socket) => {
           <AppProvider>
             <Provider store={store}>
               <SocketProvider socket={socket}>
-                <App />
+                <I18nextProvider i18n={i18instance}>
+                  <App />
+                </I18nextProvider>
               </SocketProvider>
             </Provider>
           </AppProvider>
